c9fe4cab464391c3cab9034001a571ac11db477b
Ben Corlett <bencorlett@me.com>
2016-04-28 13:38:58 +1000

    Extract billing / shipping serialisation into it's own trait (orders will use this as well).

---
+++ src/Customers/V2BillingAndShippingContactSerializer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace RetailExpress\SkyLink\Customers;
+
+trait V2BillingAndShippingContactSerializer
+{
+    public function serializeBillingAndShippingContacts(BillingContact $billingContact, ShippingContact $shippingContact)
+    {
+        $payload = [];
+
+        $payload['BillFirstName'] = (string) $billingContact->getName()->getFirstName();
+        $payload['BillLastName'] = (string) $billingContact->getName()->getLastName();
+
+        $payload['BillAddress'] = (string) $billingContact->getAddress()->getLine1();
+        $payload['BillAddress2'] = (string) $billingContact->getAddress()->getLine2();
+        $payload['BillCompany'] = (string) $billingContact->getCompanyName();
+        $payload['BillSuburb'] = (string) $billingContact->getAddress()->getCity();
+        $payload['BillPostCode'] = (string) $billingContact->getAddress()->getPostcode();
+        $payload['BillState'] = (string) $billingContact->getAddress()->getState();
+        $payload['BillCountry'] = (string) $billingContact->getAddress()->getCountry();
+
+        $payload['BillEmail'] = (string) $billingContact->getEmailAddress();
+
+        $payload['DelName'] = (string) $shippingContact->getName()->getFirstName();
+        $shippingLastName = (string) $shippingContact->getName()->getLastName();
+        if ($shippingLastName !== '') {
+            $payload['DelName'] .= " {$shippingLastName}";
+        }
+
+        $payload['DelAddress'] = (string) $shippingContact->getAddress()->getLine1();
+        $payload['DelAddress2'] = (string) $shippingContact->getAddress()->getLine2();
+        $payload['DelSuburb'] = (string) $shippingContact->getAddress()->getCity();
+        $payload['DelPostCode'] = (string) $shippingContact->getAddress()->getPostcode();
+        $payload['DelState'] = (string) $shippingContact->getAddress()->getState();
+        $payload['DelCountry'] = (string) $shippingContact->getAddress()->getCountry();
+
+        return $payload;
+    }
+}
--- src/Customers/V2CustomerSerializer.php
+++ src/Customers/V2CustomerSerializer.php
@@ -6,6 +6,8 @@ use Sabre\Xml\Writer as XmlWriter;
 
 trait V2CustomerSerializer
 {
+    use V2BillingAndShippingContactSerializer;
+
     public function xmlSerialize(XmlWriter $xmlWriter)
     {
         $payload = [];
@@ -18,25 +20,7 @@ trait V2CustomerSerializer
             $payload['Password'] = (string) $this->getPassword();
         }
 
-        $payload['BillFirstName'] = (string) $this->getBillingContact()->getName()->getFirstName();
-        $payload['BillLastName'] = (string) $this->getBillingContact()->getName()->getLastName();
-
-        $payload['BillAddress'] = (string) $this->getBillingContact()->getAddress()->getLine1();
-        $payload['BillAddress2'] = (string) $this->getBillingContact()->getAddress()->getLine2();
-        $payload['BillCompany'] = (string) $this->getBillingContact()->getCompanyName();
-        $payload['BillSuburb'] = (string) $this->getBillingContact()->getAddress()->getCity();
-        $payload['BillPostCode'] = (string) $this->getBillingContact()->getAddress()->getPostcode();
-        $payload['BillState'] = (string) $this->getBillingContact()->getAddress()->getState();
-        $payload['BillCountry'] = (string) $this->getBillingContact()->getAddress()->getCountry();
-
-        $payload['BillEmail'] = (string) $this->getBillingContact()->getEmailAddress();
-
-        $payload['DelAddress'] = (string) $this->getShippingContact()->getAddress()->getLine1();
-        $payload['DelAddress2'] = (string) $this->getShippingContact()->getAddress()->getLine2();
-        $payload['DelSuburb'] = (string) $this->getShippingContact()->getAddress()->getCity();
-        $payload['DelPostCode'] = (string) $this->getShippingContact()->getAddress()->getPostcode();
-        $payload['DelState'] = (string) $this->getShippingContact()->getAddress()->getState();
-        $payload['DelCountry'] = (string) $this->getShippingContact()->getAddress()->getCountry();
+        $payload = array_merge($payload, $this->serializeBillingAndShippingContacts($this->getBillingContact(), $this->getShippingContact()));
 
         $payload['ReceivesNews'] = (string) $this->getNewsletterSubscription();
 
