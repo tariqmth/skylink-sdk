393e51d5381410ff4a9bed75b6a769e5ae10e943
Ben Corlett <bencorlett@me.com>
2016-11-08 13:54:30 +1100

    Asserting that product type is not used as part of a matrix policy.
    This is due to how its’ values are not exposed by matrices if it’s part of a matrix policy, but its’ values are always needed.

--- src/Catalogue/Products/MatrixPolicy.php
+++ src/Catalogue/Products/MatrixPolicy.php
@@ -4,6 +4,7 @@ namespace RetailExpress\SkyLink\Sdk\Catalogue\Products;
 
 use BadMethodCallException;
 use RetailExpress\SkyLink\Sdk\Catalogue\Attributes\Attribute;
+use RetailExpress\SkyLink\Sdk\Catalogue\Attributes\AttributeCode;
 
 class MatrixPolicy
 {
@@ -32,6 +33,8 @@ class MatrixPolicy
     public function __construct(array $attributes)
     {
         $this->attributes = array_map(function (Attribute $attribute) {
+            $this->assertAttributeIsAllowed($attribute);
+
             return $attribute;
         }, $attributes);
     }
@@ -41,5 +44,14 @@ class MatrixPolicy
         return array_map(function (Attribute $attribute) {
             return clone $attribute;
         }, $this->attributes);
+    }
+
+    private function assertAttributeIsAllowed(Attribute $attribute)
+    {
+        $notAllowed = AttributeCode::fromNative('product_type');
+
+        if ($attribute->getCode()->samevalueAs($notAllowed)) {
+            throw new \InvalidArgumentException("Attribute \"{$attribute->getCode()}\" is not allowed in a Matrix policy.");
+        }
     }
 }
