7705324f1d6538c9f0d03196e3a68f6cc534cfcb
Ben Corlett <bencorlett@me.com>
2016-12-01 14:13:46 +1000

    Add method to convert back from V2 status.

--- src/Sales/Orders/V2StatusConverter.php
+++ src/Sales/Orders/V2StatusConverter.php
@@ -2,6 +2,8 @@
 
 namespace RetailExpress\SkyLink\Sdk\Sales\Orders;
 
+use UnexpectedValueException;
+
 trait V2StatusConverter
 {
     private static $mappings = [
@@ -18,5 +20,16 @@ trait V2StatusConverter
     public function toV2Status()
     {
         return self::$mappings[$this->getValue()];
+    }
+
+    public static function fromV2Status($v2Status)
+    {
+        $key = array_search($v2Status, self::$mappings);
+
+        if (false == $key) {
+            throw new UnexpectedValueException("Invalid V2 Status {$v2Status}.");
+        }
+
+        return self::get($key);
     }
 }
