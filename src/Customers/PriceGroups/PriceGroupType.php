2327274588190efa2ac625853223f4b5ba34e506
Ben Corlett <bencorlett@me.com>
2017-02-23 16:41:30 +1100

    Move responsibility for displaying a price group type’s name to itself.

--- src/Customers/PriceGroups/PriceGroupType.php
+++ src/Customers/PriceGroups/PriceGroupType.php
@@ -3,9 +3,23 @@
 namespace RetailExpress\SkyLink\Sdk\Customers\PriceGroups;
 
 use ValueObjects\Enum\Enum;
+use ValueObjects\StringLiteral\StringLiteral;
 
 class PriceGroupType extends Enum
 {
     const STANDARD = 'standard';
     const FIXED = 'fixed';
+
+    /**
+     * @return StringLiteral
+     */
+    public function getPriceGroupName()
+    {
+        $names = [
+            self::STANDARD => 'Standard',
+            self::FIXED => 'Fixed',
+        ];
+
+        return new StringLiteral($names[$this->getValue()]);
+    }
 }
