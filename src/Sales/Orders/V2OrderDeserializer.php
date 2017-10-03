tariqmth/skylink-sdk

commit ceeb661fa3af43854621176c11b58c519d319789

Add compatibility with Retail Express’s hard-coded Sydney timezone. [touch:8]

src/Sales/Orders/V2OrderDeserializer.php
@@ -2,7 +2,6 @@
 
 namespace RetailExpress\SkyLink\Sdk\Sales\Orders;
 
-use DateTimeImmutable;
 use RetailExpress\SkyLink\Sdk\Customers\BillingContact;
 use RetailExpress\SkyLink\Sdk\Customers\CustomerId;
 use RetailExpress\SkyLink\Sdk\Customers\ShippingContact;
@@ -19,7 +18,7 @@ class V2OrderDeserializer implements XmlDeserializable
 
         $customerId = new CustomerId($payload['CustomerId']);
 
-        $placedAt = new DateTimeImmutable($payload['DateCreated']);
+        $placedAt = from_v2_rex_date($payload['CreatedAt']);
 
         $status = Status::fromV2Status($payload['OrderStatus']);
 

src/Catalogue/Eta/V2EtaDeserializer.php
@@ -19,7 +19,7 @@ trait V2EtaDeserializer
         return self::fromNative(
             $payload['ProductID'],
             $payload['QtyOrdered'],
-            strtotime($payload['ETA'])
+            from_v2_rex_date_to_timestamp($payload['ETA'])
         );
     }
 }

src/Catalogue/Products/V2ProductDeserializer.php
@@ -172,7 +172,7 @@ class V2ProductDeserializer
             return null;
         }
 
-        return strtotime($date);
+        return from_v2_rex_date_to_timestamp($date);
     }
 
     private function extractPriceGroupPrices(array $payload)

src/Sales/Fulfillments/V2FulfillmentDeserializer.php
@@ -15,7 +15,7 @@ class V2FulfillmentDeserializer implements XmlDeserializable
         $fulfillment = Fulfillment::fromNative(
             $payload['OrderId'],
             $payload['OrderItemId'],
-            strtotime($payload['DateFulfilled']),
+            from_v2_rex_date_to_timestamp($payload['DateFulfilled']),
             $payload['QtyFulfilled']
         );
 

src/Sales/Orders/V2OrderSerializer.php
@@ -13,7 +13,7 @@ trait V2OrderSerializer
     {
         $payload = [];
 
-        $payload['DateCreated'] = $this->getPlacedAt()->format(V2_API_DATE_FORMAT);
+        $payload['DateCreated'] = to_v2_rex_date($this->getPlacedAt());
         $payload['OrderTotal'] = (string) $this->getTotal();
         $payload['FreightTotal'] = (string) $this->getShippingCharge()->getPrice();
         $payload['OrderStatus'] = $this->getStatus()->toV2Status();

src/Sales/Payments/V2PaymentDeserializer.php
@@ -15,7 +15,7 @@ class V2PaymentDeserializer implements XmlDeserializable
 
         $payment = Payment::normalFromNative(
             $payload['OrderId'],
-            strtotime($payload['DateCreated']),
+            from_v2_rex_date_to_timestamp($payload['DateCreated']),
             $payload['MethodId'],
             $payload['Total']
         );

src/Sales/Payments/V2PaymentSerializer.php
@@ -13,7 +13,7 @@ trait V2PaymentSerializer
         $payload['OrderId'] = (string) $this->getOrderId();
         $payload['MethodId'] = (string) $this->getMethodId();
         $payload['Amount'] = (string) $this->getTotal();
-        $payload['DateCreated'] = $this->getMadeAt()->format(V2_API_DATE_FORMAT);
+        $payload['DateCreated'] = to_v2_rex_date($this->getMadeAt());
 
         if ($this->usesVoucherCode()) {
             $payload['VoucherCode'] = (string) $this->getVoucherCode();

src/helpers.php
@@ -4,6 +4,43 @@ use RetailExpress\SkyLink\Sdk\Util;
 
 // The format of dates in the V2 API (maybe move this?)
 const V2_API_DATE_FORMAT = 'Y-m-d\TH:i:s.000';
+const V2_API_TIMEZONE = 'Australia/Sydney';
+
+/**
+ * Takes a Date Time object and converts it to a formatted Retail Express date, in Sydne time.
+ *
+ * @return string
+ */
+function to_v2_rex_date(DateTimeImmutable $date)
+{
+    return $date->setTimezone(new DateTimeZone(V2_API_TIMEZONE))->format(V2_API_DATE_FORMAT);
+}
+
+/**
+ * Takes a date from Retail Express' V2 API (which is usually, but not always, in the format
+ * above), which is always in Sydney time, and converts it to a UTC date.
+ *
+ * @param string $date
+ *
+ * @return DateTimeImmutable
+ */
+function from_v2_rex_date($date)
+{
+    return new DateTimeImmutable($date, new DateTimeZone(V2_API_TIMEZONE));
+}
+
+
+/**
+ * Takes a date from Retail Express, converts it to UTC and provides a timestamp.
+ *
+ * @param string $date
+ *
+ * @return int $timestamp
+ */
+function from_v2_rex_date_to_timestamp($date)
+{
+    return from_v2_rex_date($date)->getTimestamp();
+}
 
 /**
  * Returns the value of the given key in an array, providing there is actually a non-empty value there.

commit ee5ef7e879ed70c19fb0f76ccb37fa08de541675

Fix an issue introduced in ceeb661f that broke deserialising orders. [Testing:42]

src/Sales/Orders/V2OrderDeserializer.php
@@ -18,7 +18,7 @@ class V2OrderDeserializer implements XmlDeserializable
 
         $customerId = new CustomerId($payload['CustomerId']);
 
-        $placedAt = from_v2_rex_date($payload['CreatedAt']);
+        $placedAt = from_v2_rex_date($payload['DateCreated']);
 
         $status = Status::fromV2Status($payload['OrderStatus']);
 